a3b72cd385a64f043cbaab00da10983cb4b0abfb
lpolverino <lorenz.polverino@gmail.com>
2024-08-03 08:42:21 -0300

    Added query skeleton

--- api.go
+++ api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -9,6 +10,12 @@ import (
 
 type APIServer struct {
 	ListenAddr string
+}
+
+type Query struct {
+	Location     string `json:"location" binding:"required"`
+	StartingDate string `json:"date_1" bson:",omitempty"`
+	EndingDate   string `json:"date_2" bson:",omitempty"`
 }
 
 func NewAPIServer(listenAddr string) *APIServer {
@@ -25,5 +32,27 @@ func (a *APIServer) Run() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.POST("/", func(c echo.Context) error {
+		queryUrl := "https://www.example.com/https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+		newQuery := Query{}
+		err := c.Bind(&newQuery)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "Cannot process query")
+		}
+		queryUrl += newQuery.Location
+
+		fmt.Printf("%+v\n", newQuery)
+		fmt.Printf("%d", len(newQuery.EndingDate))
+
+		if len(newQuery.StartingDate) != 0 && len(newQuery.EndingDate) != 0 {
+			queryUrl += "/" + newQuery.StartingDate + "/" + newQuery.EndingDate
+		} else if !(len(newQuery.StartingDate) == 0 && len(newQuery.EndingDate) == 0) {
+			return c.String(http.StatusBadRequest, "cannot process query")
+		}
+
+		return c.String(http.StatusAccepted, queryUrl)
+	})
+
 	e.Logger.Fatal(e.Start(":" + a.ListenAddr))
 }
